6b1c7bbea730a32f3b2c499036189bfeadfef2de
Eric Jacobsen <eric@whiskyvangoghgo.com>
2025-10-05 17:11:44 -0400

    embed version

--- class_spill.js
+++ class_spill.js
@@ -1,97 +1,88 @@
 class Spill {
-  constructor(x, y, spiller){
-    this.xOrg = x;
-    this.yOrg = y;
+  constructor(x, y, spiller) {
+    this.xOrg = x
+    this.yOrg = y
 
-    this.spiller = spiller;
+    this.spiller = spiller
 
-    this.p1 = createVector(this.xOrg, this.yOrg);
-    this.p2 = createVector(this.xOrg, this.yOrg);
-    this.p3 = createVector(this.xOrg, this.yOrg);
-    this.p4 = createVector(this.xOrg, this.yOrg);
-    this.p5 = createVector(this.xOrg, this.yOrg);
+    this.p1 = createVector(this.xOrg, this.yOrg)
+    this.p2 = createVector(this.xOrg, this.yOrg)
+    this.p3 = createVector(this.xOrg, this.yOrg)
+    this.p4 = createVector(this.xOrg, this.yOrg)
+    this.p5 = createVector(this.xOrg, this.yOrg)
 
-    this.ang0;
-    this.t1 = createVector(this.xOrg, this.yOrg - underHeight);
-    this.t2 = createVector(this.xOrg, this.yOrg + underHeight);
+    this.ang0
+    this.t1 = createVector(this.xOrg, this.yOrg - underHeight)
+    this.t2 = createVector(this.xOrg, this.yOrg + underHeight)
 
-    this.sw = 0;
-    this.swOrg = sandRes * swFactor;
-    this.swTar = 25;
+    this.sw = 0
+    this.swOrg = sandRes * swFactor
+    this.swTar = 25
 
-    this.indA = 0;
-    this.indB = 0;
+    this.indA = 0
+    this.indB = 0
 
-    this.ticker = 0;
-    
-    this.build();
+    this.ticker = 0
+
+    this.build()
   }
 
-  build(){
-    this.ticker = 0;
+  build() {
+    this.ticker = 0
 
-    if(this.spiller){
-      this.swOrg = sandRes * swFactor;
+    if (this.spiller) {
+      this.swOrg = sandRes * swFactor
     } else {
-      this.swOrg = 0;
+      this.swOrg = 0
     }
-    this.swTar = sandRes * 2;
+    this.swTar = sandRes * 2
 
     // INTRO STAGGER
-    if(animPreset0 == 0){
-      var tk0 = dist(this.xOrg, this.yOrg, underWidth/2, underHeight/2);
-      this.indA = constrain(map(tk0, 0, underWidth/2, animA, 0), 0, animA);
-      this.t1.set(this.xOrg, -150);
-    
-    } else if(animPreset0 == 1){
-      var tk0 = dist(0, this.yOrg, 0, underHeight/2 - fullH/2);
-      this.indA = constrain(map(tk0, 0, underHeight, 0, animA), 0, animA);
-      this.t1.set(this.xOrg, -150);
-
-    } else if(animPreset0 == 2){
-      if(this.xOrg < underWidth/2){
-        var tk0 = dist(this.xOrg, 0, underWidth/2 - fullW/2, 0);
-        this.indA = constrain(map(tk0, 0, fullW/2, 0, animA), 0, animA);
-        this.t1.set(-150, this.yOrg);
-
+    if (animPreset0 == 0) {
+      var tk0 = dist(this.xOrg, this.yOrg, underWidth / 2, underHeight / 2)
+      this.indA = constrain(map(tk0, 0, underWidth / 2, animA, 0), 0, animA)
+      this.t1.set(this.xOrg, -150)
+    } else if (animPreset0 == 1) {
+      var tk0 = dist(0, this.yOrg, 0, underHeight / 2 - fullH / 2)
+      this.indA = constrain(map(tk0, 0, underHeight, 0, animA), 0, animA)
+      this.t1.set(this.xOrg, -150)
+    } else if (animPreset0 == 2) {
+      if (this.xOrg < underWidth / 2) {
+        var tk0 = dist(this.xOrg, 0, underWidth / 2 - fullW / 2, 0)
+        this.indA = constrain(map(tk0, 0, fullW / 2, 0, animA), 0, animA)
+        this.t1.set(-150, this.yOrg)
       } else {
-        var tk0 = dist(this.xOrg, 0, underWidth/2 + fullW/2, 0);
-        this.indA = constrain(map(tk0, 0, fullW/2, 0, animA), 0, animA);
-        this.t1.set(underWidth + 150, this.yOrg);
-
+        var tk0 = dist(this.xOrg, 0, underWidth / 2 + fullW / 2, 0)
+        this.indA = constrain(map(tk0, 0, fullW / 2, 0, animA), 0, animA)
+        this.t1.set(underWidth + 150, this.yOrg)
       }
-
     }
 
     // OUTRO STAGGER
-    if(animPreset1 == 0){
-      var tk0 = dist(this.xOrg, this.yOrg, underWidth/2, underHeight/2);
-      this.indB = constrain(map(tk0, 0, underWidth/2, 0, animA), 0, animA);
-      this.t2.set(this.xOrg, underHeight + 150);
-
-    } else if(animPreset1 == 1){
-      var tk0 = dist(0, this.yOrg, 0, underHeight/2 - fullH/2);
-      this.indB = constrain(map(tk0, 0, underHeight, animA, 0), 0, animA);
-      this.t2.set(this.xOrg, underHeight + 150);
-
-    } else if(animPreset1 == 2){
-      if(this.xOrg < underWidth/2){
-        var tk0 = dist(this.xOrg, this.yOrg, underWidth/2 - fullW/2, underHeight/2);
-        this.indB = constrain(map(tk0, 0, fullW/2, animA, 0), 0, animA);
-        this.t2.set(underWidth + 150, this.yOrg);
+    if (animPreset1 == 0) {
+      var tk0 = dist(this.xOrg, this.yOrg, underWidth / 2, underHeight / 2)
+      this.indB = constrain(map(tk0, 0, underWidth / 2, 0, animA), 0, animA)
+      this.t2.set(this.xOrg, underHeight + 150)
+    } else if (animPreset1 == 1) {
+      var tk0 = dist(0, this.yOrg, 0, underHeight / 2 - fullH / 2)
+      this.indB = constrain(map(tk0, 0, underHeight, animA, 0), 0, animA)
+      this.t2.set(this.xOrg, underHeight + 150)
+    } else if (animPreset1 == 2) {
+      if (this.xOrg < underWidth / 2) {
+        var tk0 = dist(this.xOrg, this.yOrg, underWidth / 2 - fullW / 2, underHeight / 2)
+        this.indB = constrain(map(tk0, 0, fullW / 2, animA, 0), 0, animA)
+        this.t2.set(underWidth + 150, this.yOrg)
       } else {
-        var tk0 = dist(this.xOrg, this.yOrg, underWidth/2 + fullW/2, underHeight/2);
-        this.indB = constrain(map(tk0, 0, fullW/2, animA, 0), 0, animA);
-        this.t2.set(-150, this.yOrg);
+        var tk0 = dist(this.xOrg, this.yOrg, underWidth / 2 + fullW / 2, underHeight / 2)
+        this.indB = constrain(map(tk0, 0, fullW / 2, animA, 0), 0, animA)
+        this.t2.set(-150, this.yOrg)
       }
       // this.t2.set(underWidth + 150, this.yOrg);
-
     }
 
-    if(offsetTexture){
-      this.indA -= map(noise(this.xOrg * 0.01, this.yOrg * 0.01), 0, 1, 0, animA * 2/3);
-      this.indB -= map(noise(this.xOrg * 0.01, this.yOrg * 0.01), 0, 1, 0, animA * 2/3);
-    
+    if (offsetTexture) {
+      this.indA -= map(noise(this.xOrg * 0.01, this.yOrg * 0.01), 0, 1, 0, (animA * 2) / 3)
+      this.indB -= map(noise(this.xOrg * 0.01, this.yOrg * 0.01), 0, 1, 0, (animA * 2) / 3)
     }
 
     // this.t1.set(this.xOrg, -150);
@@ -100,112 +91,104 @@ class Spill {
     // this.t2.set(this.xOrg, this.yOrg + underHeight);
   }
 
-  tweak(){
-    if(this.spiller){
-      this.swOrg = sandRes * swFactor;
+  tweak() {
+    if (this.spiller) {
+      this.swOrg = sandRes * swFactor
     } else {
-      this.swOrg = 0;
+      this.swOrg = 0
     }
-    this.swTar = sandRes * 2;
+    this.swTar = sandRes * 2
   }
 
-  reset(){
-    this.ticker = 0;
+  reset() {
+    this.ticker = 0
 
-    this.p1 = createVector(this.xOrg, this.yOrg - underHeight);
-    this.p2 = createVector(this.xOrg, this.yOrg - underHeight);
-    this.p3 = createVector(this.xOrg, this.yOrg - underHeight);
+    this.p1 = createVector(this.xOrg, this.yOrg - underHeight)
+    this.p2 = createVector(this.xOrg, this.yOrg - underHeight)
+    this.p3 = createVector(this.xOrg, this.yOrg - underHeight)
     // this.p4 = createVector(this.xOrg, -100);
     // this.p5 = createVector(this.xOrg, -100);
   }
 
-  run(){
-    this.update();
-    this.display();
+  run() {
+    this.update()
+    this.display()
   }
 
-  update(){
-    var tarX = this.t1.x;
-    var tarY = this.t1.y;
+  update() {
+    var tarX = this.t1.x
+    var tarY = this.t1.y
 
-    if(this.ticker < 0){
-      tarX = this.t1.x;
-      tarY = this.t1.y;
-      this.sw = this.swOrg;
+    if (this.ticker < 0) {
+      tarX = this.t1.x
+      tarY = this.t1.y
+      this.sw = this.swOrg
+    } else if (this.ticker <= animA - this.indA) {
+      var tk0 = map(this.ticker, 0, animA - this.indA - 1, 0, 1)
+      var tk1 = easeInOutExpo(tk0)
 
-    } else if (this.ticker <= animA - this.indA){
-      var tk0 = map(this.ticker, 0, animA - this.indA - 1, 0, 1);
-      var tk1 = easeInOutExpo(tk0);
+      tarX = map(tk1, 0, 1, this.t1.x, this.xOrg)
+      tarY = map(tk1, 0, 1, this.t1.y, this.yOrg)
+      this.sw = this.swOrg
+    } else if (this.ticker < animB + this.indB) {
+      tarX = this.xOrg
+      tarY = this.yOrg
+      this.sw = this.swOrg
+    } else if (this.ticker < animC) {
+      var tk0 = map(this.ticker, animB + this.indB, animC - this.indA / 3, 0, 1)
+      var tk1 = easeInOutExpo(tk0)
 
-      tarX = map(tk1, 0, 1, this.t1.x, this.xOrg);
-      tarY = map(tk1, 0, 1, this.t1.y, this.yOrg);
-      this.sw = this.swOrg;
-
-    } else if(this.ticker < animB + this.indB){    
-      tarX = this.xOrg;
-      tarY = this.yOrg;
-      this.sw = this.swOrg;
-
-    } else if(this.ticker <  animC){
-      var tk0 = map(this.ticker, animB + this.indB,  animC - this.indA/3, 0, 1);
-      var tk1 = easeInOutExpo(tk0);
-
-      if(animMode == 0){
-        tarX = map(tk1, 0, 1, this.xOrg, this.t2.x);
-        tarY = map(tk1, 0, 1, this.yOrg, this.t2.y);
-        this.sw = this.swOrg;
-
+      if (animMode == 0) {
+        tarX = map(tk1, 0, 1, this.xOrg, this.t2.x)
+        tarY = map(tk1, 0, 1, this.yOrg, this.t2.y)
+        this.sw = this.swOrg
       } else {
-        tarX = this.xOrg;
-        tarY = this.yOrg;
-        this.sw = map(tk1, 0, 1, this.swOrg, this.swTar);
-
+        tarX = this.xOrg
+        tarY = this.yOrg
+        this.sw = map(tk1, 0, 1, this.swOrg, this.swTar)
       }
+    } else if (this.ticker < animD) {
+      var tk0 = map(this.ticker, animC - this.indA / 3, animD, 0, 1)
+      var tk1 = easeInOutExpo(tk0)
 
-    } else if(this.ticker < animD){
-      var tk0 = map(this.ticker, animC - this.indA/3, animD, 0, 1);
-      var tk1 = easeInOutExpo(tk0);
-
-      if(animMode == 0){
+      if (animMode == 0) {
         tarX = this.t2.x
         tarY = this.t2.y
-        this.sw = this.swOrg;
-
+        this.sw = this.swOrg
       } else {
-        tarX = this.xOrg;
-        tarY = this.yOrg;
-        this.sw = map(tk1, 0, 1, this.swTar, 0);
-
+        tarX = this.xOrg
+        tarY = this.yOrg
+        this.sw = map(tk1, 0, 1, this.swTar, 0)
       }
-
     } else {
-      this.reset();
+      let newAnimPreset = animPreset0 + 1
+      if (newAnimPreset > 2) {
+        newAnimPreset = 0
+      }
+      setAnimPreset0(newAnimPreset)
+      setAnimPreset1(newAnimPreset)
+      this.reset()
     }
 
-    this.ticker ++;
+    this.ticker++
 
-
-    if(this.ticker > 1){
+    if (this.ticker > 1) {
       // this.p5.set(this.p4.x, this.p4.y)
       // this.p4.set(this.p3.x, this.p3.y)
-      this.p3.set(this.p2.x, this.p2.y);
-      this.p2.set(this.p1.x, this.p1.y);
-      // this.p1.set(this.x, this.y);    
-      this.p1.set(tarX, tarY);
-
+      this.p3.set(this.p2.x, this.p2.y)
+      this.p2.set(this.p1.x, this.p1.y)
+      // this.p1.set(this.x, this.y);
+      this.p1.set(tarX, tarY)
     } else {
-      this.p3.set(tarX, tarY);
-      this.p2.set(tarX, tarY);
-      this.p1.set(tarX, tarY);
-
+      this.p3.set(tarX, tarY)
+      this.p2.set(tarX, tarY)
+      this.p1.set(tarX, tarY)
     }
-
-
   }
 
-  display(){
-    noFill();
-    stroke(foreColor);
+  display() {
+    noFill()
+    stroke(foreColor)
 
     // if(this.ticker < 5){
     //   strokeWeight(0);
@@ -214,15 +197,15 @@ class Spill {
     // } else {
     //   strokeWeight(this.sw);
     // }
-    strokeWeight(this.sw);
+    strokeWeight(this.sw)
     // stroke(colorSet[4]);
     // line(this.p4.x, this.p4.y, this.p5.x, this.p5.y);
-    
+
     // stroke(colorSet[2]);
     // line(this.p3.x, this.p3.y, this.p4.x, this.p4.y);
-    
+
     // stroke(colorSet[0]);
     // line(this.p2.x, this.p2.y, this.p3.x, this.p3.y);
-    line(this.p1.x, this.p1.y, this.p3.x, this.p3.y);
+    line(this.p1.x, this.p1.y, this.p3.x, this.p3.y)
   }
 }
--- sketch_waterfall.js
+++ sketch_waterfall.js
@@ -1,203 +1,202 @@
-var bkgdColor, foreColor;
-var colorSet = [];
+var bkgdColor, foreColor
+var colorSet = []
 
-var saveFileType = 0;
+var saveFileType = 0
 
-var sizeType = 0;
-var previewScaler = 1;
+var sizeType = 3
+var previewScaler = 1
 
-var starterText = "INSCRIPT\nTYPE+TECH\nFESTIVAL";
+var starterText = 'INSCRIPT\nTYPE+TECH\nFESTIVAL'
 
-var inputText = [];
+var inputText = []
 
-var corePG;
-var pg;
-var tFont = [];
-var tFontFactor = [];
-var pgTextSize = 300;
-var scaler = 0.9;
-var leading = 10;
-var tracking = 0;
+var corePG
+var pg
+var tFont = []
+var tFontFactor = []
+var pgTextSize = 300
+var scaler = 0.9
+var leading = 10
+var tracking = 0
 
-var sands = [];
-var sandRes = 10;
+var sands = []
+var sandRes = 10
 
-var resolveWin = 0;
-var animA = 60;
-var animB = animA + 30;
-var animC = animB + 60;
-var animD = animC + resolveWin;
+var resolveWin = 0
+var animA = 60
+var animB = animA + 30
+var animC = animB + 60
+var animD = animC + resolveWin
 
-var animPreset0 = 0;
-var animPreset1 = 0;
-var offsetTexture = true;
-var animMode = 0;
+var animPreset0 = 0
+var animPreset1 = 0
+var offsetTexture = true
+var animMode = 0
 
-var underWidth, underHeight;
+var underWidth, underHeight
 
-var customW = 1080;
-var customH = 1350;
+var customW = 1080
+var customH = 1350
 
-var defaultImage, userImage;
-var sizedImage;
-var imageLoaded = false;
-var mainGraphic;
-var imageOnlyToggle = false;
-var imageForeToggle = false;
-var imageInvertToggle = false;
+var defaultImage, userImage
+var sizedImage
+var imageLoaded = false
+var mainGraphic
+var imageOnlyToggle = false
+var imageForeToggle = false
+var imageInvertToggle = false
 
-var userSize = 1;
-var userXoffset = 0;
-var userYoffset = 0;
+var userSize = 1
+var userXoffset = 0
+var userYoffset = 0
 
-var swFactor = 0.7;
+var swFactor = 0.7
 
-var cwidth, cheight;
-var thisDensity = 1;
-var recMessageOn = false;
-var recording = false;
-var recordedFrames = 0;
-var numFrames = 150;
-var frate = 30;
+var cwidth, cheight
+var thisDensity = 1
+var recMessageOn = false
+var recording = false
+var recordedFrames = 0
+var numFrames = 150
+var frate = 30
 
-var debugToggle = false;
+var debugToggle = false
 
-var mainCanvas;
-var canvasContainer; 
+var mainCanvas
+var canvasContainer
 
-var staticSaving = false;
-var animateToggle = false;
+var staticSaving = false
+var animateToggle = false
 
-var staggerX = true;
-var staggerY = false;
+var staggerX = true
+var staggerY = false
 
-var selFont = 0;
+var selFont = 0
 
-function preload(){
+function preload() {
   // tFont[0] = loadFont("resources/Inter-Medium.ttf");
   // tFont[1] = loadFont("resources/Geist-ExtraBold.ttf");
   // tFont[2] = loadFont("resources/Evans-Narrow-Bold-Italic.otf");
   // tFont[3] = loadFont("resources/Heading-Now-45-Medium.otf");
   // tFont[4] = loadFont("resources/NeueWorld-CondensedRegular.ttf");
-  tFont[0] = loadFont("resources/Spectral-ExtraBold Italic-Desktop.otf");
-  tFont[1] = loadFont("resources/PPEditorialNew-UltraboldItalic.otf");
-  tFont[2] = loadFont("resources/NewSpirit-BoldCondensed.otf");
-  tFont[3] = loadFont("resources/FETTEFRA.TTF");
-  tFont[4] = loadFont("resources/Times New Roman Bold Italic.ttf");
-  tFont[5] = loadFont("resources/Comic Sans MS Bold.ttf");
+  tFont[0] = loadFont('resources/Spectral-ExtraBold Italic-Desktop.otf')
+  tFont[1] = loadFont('resources/PPEditorialNew-UltraboldItalic.otf')
+  tFont[2] = loadFont('resources/NewSpirit-BoldCondensed.otf')
+  tFont[3] = loadFont('resources/FETTEFRA.TTF')
+  tFont[4] = loadFont('resources/Times New Roman Bold Italic.ttf')
+  tFont[5] = loadFont('resources/Comic Sans MS Bold.ttf')
 
-  tFont[6] = loadFont("resources/ManukaCondensed-Bold.otf");
-  tFont[7] = loadFont("resources/ManukaCondensed-Black.otf");
-  tFont[8] = loadFont("resources/ManukaCondensed-Ultra.otf");
-  tFont[9] = loadFont("resources/SohneSchmal-Dreiviertelfett.otf");
-  tFont[10] = loadFont("resources/Superheat-ExtraBold.otf");
-  tFont[11] = loadFont("resources/Superheat-Black.otf");
-  tFont[12] = loadFont("resources/Triade-Upright.otf");
-  tFont[13] = loadFont("resources/Triade-Slanted.otf");
-  tFont[14] = loadFont("resources/Triade-Backslant.otf");
+  tFont[6] = loadFont('resources/ManukaCondensed-Bold.otf')
+  tFont[7] = loadFont('resources/ManukaCondensed-Black.otf')
+  tFont[8] = loadFont('resources/ManukaCondensed-Ultra.otf')
+  tFont[9] = loadFont('resources/SohneSchmal-Dreiviertelfett.otf')
+  tFont[10] = loadFont('resources/Superheat-ExtraBold.otf')
+  tFont[11] = loadFont('resources/Superheat-Black.otf')
+  tFont[12] = loadFont('resources/Triade-Upright.otf')
+  tFont[13] = loadFont('resources/Triade-Slanted.otf')
+  tFont[14] = loadFont('resources/Triade-Backslant.otf')
 
   // tFontFactor[0] = 0.75;
   // tFontFactor[1] = 0.75;
   // tFontFactor[2] = 0.8;
   // tFontFactor[3] = 0.75;
   // tFontFactor[4] = 0.72;
-  tFontFactor[0] = 0.7;
-  tFontFactor[1] = 0.8;
-  tFontFactor[2] = 0.75;
-  tFontFactor[3] = 0.9;
-  tFontFactor[4] = 0.72;
-  tFontFactor[5] = 0.8;
+  tFontFactor[0] = 0.7
+  tFontFactor[1] = 0.8
+  tFontFactor[2] = 0.75
+  tFontFactor[3] = 0.9
+  tFontFactor[4] = 0.72
+  tFontFactor[5] = 0.8
 
-  tFontFactor[6] = 0.74;
-  tFontFactor[7] = 0.74;
-  tFontFactor[8] = 0.74;
-  tFontFactor[9] = 0.72;
-  tFontFactor[10] = 0.72;
-  tFontFactor[11] = 0.72;
-  tFontFactor[12] = 0.73;
-  tFontFactor[13] = 0.73;
-  tFontFactor[14] = 0.73;
+  tFontFactor[6] = 0.74
+  tFontFactor[7] = 0.74
+  tFontFactor[8] = 0.74
+  tFontFactor[9] = 0.72
+  tFontFactor[10] = 0.72
+  tFontFactor[11] = 0.72
+  tFontFactor[12] = 0.73
+  tFontFactor[13] = 0.73
+  tFontFactor[14] = 0.73
 
-  defaultImage = loadImage("resources/glencoe.jpg");
-  sizedImage = defaultImage;
-  userImage = null;
+  defaultImage = loadImage('resources/glencoe.jpg')
+  sizedImage = defaultImage
+  userImage = null
 }
 
-function setup(){
-  canvasContainer = document.getElementById('canvas-container');
+function setup() {
+  canvasContainer = document.getElementById('canvas-container')
 
-  mainCanvas = createCanvas(windowWidth, windowHeight);
+  mainCanvas = createCanvas(windowWidth, windowHeight)
 
-  mainCanvas.parent(canvasContainer);
+  mainCanvas.parent(canvasContainer)
 
-  document.getElementById("textArea").value = starterText;
+  document.getElementById('textArea').value = starterText
 
-  bkgdColor = color("#000000");
-  foreColor = color("#ffffff");
-  colorSet[0] = color('#ffffff');
-  colorSet[1] = color('#f20f0f');
-  colorSet[2] = color('#36bf7f');
-  colorSet[3] = color('#491bf2');
-  colorSet[4] = color('#6d1bbf');
+  bkgdColor = color('#000000')
+  foreColor = color('#ffffff')
+  colorSet[0] = color('#ffffff')
+  colorSet[1] = color('#f20f0f')
+  colorSet[2] = color('#36bf7f')
+  colorSet[3] = color('#491bf2')
+  colorSet[4] = color('#6d1bbf')
 
-  wWindowMin = underWidth/8,
-  wWindowMax = underWidth;
-  wWindow = map(scaler, 0, 1, wWindowMin, wWindowMax);
+  ;(wWindowMin = underWidth / 8), (wWindowMax = underWidth)
+  wWindow = map(scaler, 0, 1, wWindowMin, wWindowMax)
 
-  windowResized();
-  sizedImage.resize(underWidth, underHeight);
+  windowResized()
+  sizedImage.resize(underWidth, underHeight)
 
-  thisDensity = pixelDensity();
-  cwidth = width;
-  cheight = height;
+  thisDensity = pixelDensity()
+  cwidth = width
+  cheight = height
 
-  rectMode(CENTER);
+  rectMode(CENTER)
   // imageMode(CENTER);
-  textureMode(NORMAL);
-  strokeCap(ROUND);
-  strokeJoin(ROUND);
+  textureMode(NORMAL)
+  strokeCap(ROUND)
+  strokeJoin(ROUND)
 
-  frameRate(frate);
+  frameRate(frate)
 
-  setText();
+  setText()
 }
 
 function draw() {
-  background(bkgdColor);
-  
+  background(bkgdColor)
+
   // console.log(previewScaler)
 
-  push();
-    translate(width/2, height/2);
-    scale(previewScaler);
-    translate(-underWidth/2, -underHeight/2);
-    
-    // translate(width/2, height/2);
-    // scale(0.2);
-    // translate(-width/2, -height/2);
+  push()
+  translate(width / 2, height / 2)
+  scale(previewScaler)
+  translate(-underWidth / 2, -underHeight / 2)
 
-    // image(corePG, 0, 0);
-    // image(sizedImage, 0, 0);
+  // translate(width/2, height/2);
+  // scale(0.2);
+  // translate(-width/2, -height/2);
 
-    // if(imageOnlyToggle){
-    //   image(mainGraphic, 0, 0);
-    // } else {
-      push();
-        for(var m = 0; m < sands.length; m++){
-          sands[m].run();
-        }
-      pop();
-    // }
+  // image(corePG, 0, 0);
+  // image(sizedImage, 0, 0);
 
-    // blend(sizedImage, 0, 0, width, height, 0, 0, width, height, SCREEN);
-    if(imageForeToggle){
-      if(imageInvertToggle){
-        blend(sizedImage, 0, 0, underWidth, underHeight, 0, 0, underWidth, underHeight, SCREEN);
-      } else {
-        blend(sizedImage, 0, 0, underWidth, underHeight, 0, 0, underWidth, underHeight, MULTIPLY);
-      }
+  // if(imageOnlyToggle){
+  //   image(mainGraphic, 0, 0);
+  // } else {
+  push()
+  for (var m = 0; m < sands.length; m++) {
+    sands[m].run()
+  }
+  pop()
+  // }
+
+  // blend(sizedImage, 0, 0, width, height, 0, 0, width, height, SCREEN);
+  if (imageForeToggle) {
+    if (imageInvertToggle) {
+      blend(sizedImage, 0, 0, underWidth, underHeight, 0, 0, underWidth, underHeight, SCREEN)
+    } else {
+      blend(sizedImage, 0, 0, underWidth, underHeight, 0, 0, underWidth, underHeight, MULTIPLY)
     }
-  pop();
+  }
+  pop()
 
   // blendMode(MULTIPLY);
   // background(255);
@@ -208,40 +207,43 @@ function draw() {
   // fill(foreColor);
   // text(round(frameRate()), 20, 20);
 
-  runRecording();
+  runRecording()
 }
 
-function analyzeText(){
-  sands = [];
+function analyzeText() {
+  sands = []
 
-  for(var m = 0; m <= (underWidth)/sandRes; m++){
-    for(var n = 0; n <= (underHeight)/sandRes; n++){
-      var x = m * sandRes;
-      if(staggerX){ x += (n%2) * sandRes/2; }
+  for (var m = 0; m <= underWidth / sandRes; m++) {
+    for (var n = 0; n <= underHeight / sandRes; n++) {
+      var x = m * sandRes
+      if (staggerX) {
+        x += ((n % 2) * sandRes) / 2
+      }
 
-      var y = n * sandRes;
-      if(staggerY){ y += (m%2) * sandRes/2; }
+      var y = n * sandRes
+      if (staggerY) {
+        y += ((m % 2) * sandRes) / 2
+      }
 
-      var getColor = corePG.get(x, y);
-      var getBright = brightness(getColor);
+      var getColor = corePG.get(x, y)
+      var getBright = brightness(getColor)
 
       // if(getBright > 10){
-        // x += width/2;
-        // y += height/2;
+      // x += width/2;
+      // y += height/2;
 
-        // x -= corePG.width/2;
-        // y -= pg.length * corePG.height/2;
+      // x -= corePG.width/2;
+      // y -= pg.length * corePG.height/2;
 
-        // y += p * corePG.height;
+      // y += p * corePG.height;
 
-        var spiller = false;
-        if(getBright > 10){
-          spiller = true;
-        }
-        sands[sands.length] = new Spill(x, y, spiller);
+      var spiller = false
+      if (getBright > 10) {
+        spiller = true
+      }
+      sands[sands.length] = new Spill(x, y, spiller)
 
       // }
     }
   }
-
 }
